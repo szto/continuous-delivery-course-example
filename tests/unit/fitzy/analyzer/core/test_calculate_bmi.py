564848951f4f04d6d693e7fc10dba48c0fc0ee74
szto <sztolabs@gmail.com>
2024-01-03 23:30:52 +0900

    test: not negative value

--- tests/unit/fitzy/analyzer/core/test_calculate_bmi.py
+++ tests/unit/fitzy/analyzer/core/test_calculate_bmi.py
@@ -5,7 +5,7 @@ from fitzy.analyzer.core import calculate_bmi
 
 def test_negative_height_should_raise_error() -> None:
     # Arrange
-    negative_height = -1
+    negative_height = 1
     dummy_weight = 10
 
     # Act, Assert
